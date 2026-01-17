29b0e083108db3da52f9fbaae560ff870dd6fe48
Tiago Oliveira - TiagoDev0708 <tiagoj0708@gmail.com>
2026-01-17 20:34:10 -0300

    feat: Adicionando serviços da castlewall a landing

--- components/views/CaseStudiesView.tsx
+++ components/views/CaseStudiesView.tsx
@@ -95,7 +95,7 @@ export function CaseStudiesView() {
           transition={{ duration: 0.6 }}
           className="text-3xl md:text-4xl lg:text-5xl font-semibold text-white mb-12 px-4 md:px-0"
         >
-          Projetos que definem nosso padrão<span className="text-[#ef4444]">.</span>
+          Projetos dos quais participamos ativamente do desenvolvimento<span className="text-[#ef4444]">.</span>
         </motion.h2>
 
         <div 
